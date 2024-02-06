59b0acddfdfc0d11edca172b96eb79bd500a5d9c
lear23 <130910243+lear23@users.noreply.github.com>
2024-02-06 18:01:35 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+using AllForApproved.Contexts;
+using AllForApproved.Repositories;
+using AllForApproved.Services;
+using ConsoleApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+
+
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+    services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\SkolaUppgifter\AllForApproved\AllForApproved\Data\DataBaseCF.mdf;Integrated Security=True;Connect Timeout=30"));
+   
+    services.AddScoped<AddressRepo>();
+    services.AddScoped<ContactRepo>();
+    services.AddScoped<CustomerRepo>();
+    services.AddScoped<CountryRepo>();
+    services.AddScoped<UserRepo>();
+
+
+    services.AddScoped<AddressService>();
+    services.AddScoped<ContactService>();
+    services.AddScoped<CustomerService>();
+    services.AddScoped<CountryService>();
+    services.AddScoped<UserService>();
+
+    
+ 
+
+    services.AddSingleton<MenuService>();
+  
+
+
+}).Build();
+
+var menuService = builder.Services.GetRequiredService<MenuService>();
+menuService.CreateCustomer_UI();
+
+
---
+++ ProductSQLTabeller.sql
@@ -0,0 +1,35 @@
+﻿
+CREATE TABLE ProductModelEntity (
+    ID INT PRIMARY KEY NOT NULL,
+    ModelName VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE ProductOrderEntity (
+    ID INT PRIMARY KEY NOT NULL,
+    OrderDate DATE NOT NULL
+);
+
+
+CREATE TABLE ProductDetailsEntity (
+    ID INT PRIMARY KEY NOT NULL,
+    Color VARCHAR(50) NOT NULL,
+    Description VARCHAR(MAX) NULL
+);
+
+
+CREATE TABLE ProductEntity (
+    ProductID INT PRIMARY KEY NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    ProductOrderEntityID INT NOT NULL REFERENCES ProductOrderEntity(ID),
+    ProductModelEntityID INT NOT NULL REFERENCES ProductModelEntity(ID),
+    ProductDetailsEntityID INT NOT NULL REFERENCES ProductDetailsEntity(ID)
+);
+
+
+CREATE TABLE CategoryEntity (
+    CategoryID INT PRIMARY KEY NOT NULL,
+    CategoryName VARCHAR(50) NOT NULL UNIQUE,
+    ProductID INT NOT NULL REFERENCES ProductEntity(ProductID)
+);
